166ad246f0b399581431c45eaacd7b54ad975bdc
Виктор <140902080+ViktorK87@users.noreply.github.com>
2025-05-10 13:52:59 +0300

    Delete API/config_API.py

--- API/config_API.py
+++
@@ -1,5 +0,0 @@
-class Config_API:
-    API_URL = "https://kinopoiskapiunofficial.tech"
-    API_key = "Подставьте свой ключ "
-    #Подставьте свой ключ 
-    #Сделать ридми с ссылкой на получение ключа
